4c4bc1f2d811212c05f6b780c80d6445e7ded702
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2024-07-31 16:28:25 -0700

    Update main.js
    Wrote code generating players along with their icons and scores, and how many times the function has been called.

--- main.js
+++ main.js
@@ -17,3 +17,66 @@ function boardGame() {
 
 // This will be used to call the board
 const getBoard = () => board;
+
+// This gives players their name and score
+// Along with also keeping track of how many players were created
+function createPlayer(name) {
+    const userName = name;
+    let hasBeenCalledTwice = false;
+    let counter = 0;
+    const icon = ['X', 'O'];
+  
+    // Loop to keep track how many times function has been called
+    // Along with assigning letter, either X or O
+    for (let i = 0; i < 3; i++) {
+        counter++
+        if (counter === 1) {
+            icon[0];
+            console.log('Player was created!');
+        } else if (counter === 2) {
+            icon[1];
+            console.log('Player Was Created')
+        } else if (counter >= 3) {
+            console.log('Only two players can be created');
+            hasBeenCalledTwice = true;
+        }
+    }
+
+    // Player Score
+    let userScore = 0;
+    const getUserScore = () => userScore;
+    const giveUserScore = () => userScore++;
+
+    return {name, counter, icon, userName, getUserScore, giveUserScore};
+
+}
+
+const steven = createPlayer('steven');
+
+steven.giveUserScore();
+
+console.log({
+    userName: steven.userName,
+    userScore: steven.getUserScore(),
+    counter: steven.counter,
+    icon: steven.icon
+});
+// // Assigning players their respective value: X and O.
+// function assignPlayerValue() {
+//     return Player.map((player, index) => {
+//         return {name: player, number: index + 1};
+//     });
+// }
+
+// const playersWithValue = assignPlayerValue(Player);
+
+// playersWithValue.forEach(player => {
+//     const playerTitle = document.createElement('h1');
+//     playerTitle.textContent = `It's ${player.name}'s turn.`;
+//     // Append child to div here later
+// });
+
+// Game Controller for how game will play out
+function gameController(playerOneName = createPlayer(), playerTwoName) {
+
+}
